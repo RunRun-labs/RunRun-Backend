97571c55070c8307d0e956ca82561df5150a2802
kyungsoo <yksoo00@naver.com>
2025-12-15 13:42:47 +0900

    chore : 스프링부트 기본 셋팅

---
+++ src/main/java/com/multi/runrunbackend/RunRunBackendApplication.java
@@ -0,0 +1,13 @@
+package com.multi.runrunbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunRunBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunRunBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/multi/runrunbackend/RunRunBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.runrunbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunRunBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
