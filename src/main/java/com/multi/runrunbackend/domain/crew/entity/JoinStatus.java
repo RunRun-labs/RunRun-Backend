ba29d59e81f84c83afcf9badde9a71dcdcdeef55
DESKTOP-P7D06C6\hbk49 <hbok491@gmail.com>
2025-12-17 20:40:53 +0900

    Feat: 크루원 역할, 크루 가입 신청 상태 enum 추가

---
+++ src/main/java/com/multi/runrunbackend/domain/crew/entity/JoinStatus.java
@@ -0,0 +1,15 @@
+package com.multi.runrunbackend.domain.crew.entity;
+
+/**
+ * @author : bokyoung
+ * @description : 가입 신청 상태 Enum (PENDING: 대기중, APPROVED: 승인, REJECTED: 거절, CANCELED: 취소)
+ * @filename : JoinStatus
+ * @since : 25. 12. 17. 수요일
+ */
+public enum JoinStatus {
+
+    PENDING,
+    APPROVED,
+    REJECTED,
+    CANCELED
+}
