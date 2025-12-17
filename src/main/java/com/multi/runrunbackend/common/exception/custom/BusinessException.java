24a2c41caf4946a028f3f7bdb6f2c787dd20df76
DESKTOP-P7D06C6\hbk49 <hbok491@gmail.com>
2025-12-17 23:50:39 +0900

    Feat: 비즈니스 예외 추가

---
+++ src/main/java/com/multi/runrunbackend/common/exception/custom/BusinessException.java
@@ -0,0 +1,15 @@
+package com.multi.runrunbackend.common.exception.custom;
+
+import com.multi.runrunbackend.common.exception.dto.ErrorCode;
+
+/**
+ * @author : BoKyung
+ * @description : Please explain the class!!!
+ * @filename : BusinessException
+ * @since : 25. 12. 17. 수요일
+ */
+public class BusinessException extends CustomException {
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
