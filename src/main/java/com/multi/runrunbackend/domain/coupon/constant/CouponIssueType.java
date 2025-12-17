54bbcb69aecb27a2368ade082fce332257aa831f
kyungsoo <yksoo00@naver.com>
2025-12-17 15:38:55 +0900

    feat: 쿠폰 엔티티 및 enum 추가

---
+++ src/main/java/com/multi/runrunbackend/domain/coupon/constant/CouponIssueType.java
@@ -0,0 +1,12 @@
+package com.multi.runrunbackend.domain.coupon.constant;
+
+/**
+ * @author : kyungsoo
+ * @description : 쿠폰 발행 타입 자동 AUTO / 수동 MANUAL
+ * @filename : CourseSiren
+ * @since : 2025. 12. 17. Wednesday
+ */
+public enum CouponIssueType {
+    AUTO,
+    MANUAL
+}
---
+++ src/main/java/com/multi/runrunbackend/domain/coupon/constant/CouponTriggerEvent.java
@@ -0,0 +1,15 @@
+package com.multi.runrunbackend.domain.coupon.constant;
+
+/**
+ * @author : kyungsoo
+ * @description : 쿠폰 트리거 이벤트   SIGNUP, LOGIN, FIRST_RUNNING, RUN_COUNT_REACHED, BIRTHDAY
+ * @filename : CourseSiren
+ * @since : 2025. 12. 17. Wednesday
+ */
+public enum CouponTriggerEvent {
+    SIGNUP,
+    LOGIN,
+    FIRST_RUNNING,
+    RUN_COUNT_REACHED,
+    BIRTHDAY
+}
