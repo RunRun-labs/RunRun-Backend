112524ceb5411f2b2cfec79f859173ab4d5ac2d6
DESKTOP-P7D06C6\hbk49 <hbok491@gmail.com>
2025-12-18 02:19:24 +0900

    Feat: 크루 생성 Request DTO 구현

---
+++ src/main/java/com/multi/runrunbackend/domain/crew/dto/CrewCreateReqDto.java
@@ -0,0 +1,67 @@
+package com.multi.runrunbackend.domain.crew.dto;
+
+import com.multi.runrunbackend.domain.crew.entity.Crew;
+import com.multi.runrunbackend.domain.user.entity.User;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author : BoKyung
+ * @description : 크루 생성 요청 DTO
+ * @filename : CrewCreateReqDto
+ * @since : 25. 12. 18. 목요일
+ */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Schema(description = "크루 생성")
+public class CrewCreateReqDto {
+
+    @Schema(description = "크루명", required = true)
+    @NotBlank(message = "크루명은 필수입니다.")
+    @Size(max = 100, message = "크루명은 100자 이내로 입력해주세요.")
+    private String crewName;
+
+    @Schema(description = "크루 소개글")
+    private String crewDescription;
+
+    @Schema(description = "크루 이미지 URL")
+    private String crewImageUrl;
+
+    @Schema(description = "지역", required = true)
+    @NotBlank(message = "지역은 필수입니다.")
+    @Size(max = 100, message = "지역은 100자 이내로 입력해주세요.")
+    private String region;
+
+    @Schema(description = "러닝 거리", required = true)
+    @NotBlank(message = "러닝 거리는 필수입니다.")
+    @Size(max = 50, message = "러닝 거리는 50자 이내로 입력해주세요.")
+    private String distance;
+
+    @Schema(description = "정기모임일시", required = true)
+    @NotBlank(message = "정기모임일시는 필수입니다.")
+    @Size(max = 100, message = "정기모임일시는 100자 이내로 입력해주세요.")
+    private String activityTime;
+
+    /**
+     * @param user 크루장
+     * @description : toEntity : DTO → Entity 변환
+     */
+    public Crew toEntity(User user) {
+        return Crew.create(
+                this.crewName,
+                this.crewDescription,
+                this.crewImageUrl,
+                this.region,
+                this.distance,
+                this.activityTime,
+                user
+        );
+    }
+}
