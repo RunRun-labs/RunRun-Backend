78f73aebb1e853422891ad446cca499b5c9be6f8
kyungsoo <yksoo00@naver.com>
2025-12-17 15:39:02 +0900

    feat: 코스 엔티티 및 enum 추가

---
+++ src/main/java/com/multi/runrunbackend/domain/course/constant/CourseStatus.java
@@ -0,0 +1,13 @@
+package com.multi.runrunbackend.domain.course.constant;
+
+/**
+ * @author : kyungsoo
+ * @description : 코스 상태  ACTIVE, BLOCKED, DELETED
+ * @filename : CourseSiren
+ * @since : 2025. 12. 17. Wednesday
+ */
+public enum CourseStatus {
+    ACTIVE,
+    BLOCKED,
+    DELETED
+}
