4d251e35c152ef655a7d0b82bb93c148c4334fe6
DESKTOP-P7D06C6\hbk49 <hbok491@gmail.com>
2025-12-17 21:01:14 +0900

    Comment:  주석 AUTHOR 변경

--- src/main/java/com/multi/runrunbackend/domain/crew/entity/CrewRole.java
+++ src/main/java/com/multi/runrunbackend/domain/crew/entity/CrewRole.java
@@ -1,7 +1,7 @@
 package com.multi.runrunbackend.domain.crew.entity;
 
 /**
- * @author : bokyoung
+ * @author : BoKyung
  * @description : 크루원 역할 Enum (LEADER: 크루장, SUB_LEADER: 부크루장, STAFF: 운영진, MEMBER: 일반크루원)
  * @filename : CrewRole
  * @since : 25. 12. 17. 수요일
--- src/main/java/com/multi/runrunbackend/domain/crew/entity/JoinStatus.java
+++ src/main/java/com/multi/runrunbackend/domain/crew/entity/JoinStatus.java
@@ -1,7 +1,7 @@
 package com.multi.runrunbackend.domain.crew.entity;
 
 /**
- * @author : bokyoung
+ * @author : BoKyung
  * @description : 가입 신청 상태 Enum (PENDING: 대기중, APPROVED: 승인, REJECTED: 거절, CANCELED: 취소)
  * @filename : JoinStatus
  * @since : 25. 12. 17. 수요일
