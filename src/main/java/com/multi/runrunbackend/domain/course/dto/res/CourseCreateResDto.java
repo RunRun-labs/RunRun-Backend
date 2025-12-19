3b17c654b33e14974dfe87e52ee4a6230abc8480
kyungsoo <yksoo00@naver.com>
2025-12-19 09:40:00 +0900

    feat: 코스 만들기 Response Dto id를 반환하여 바로 상세가기로 갈 수 있게 했다

---
+++ src/main/java/com/multi/runrunbackend/domain/course/dto/res/CourseCreateResDto.java
@@ -0,0 +1,21 @@
+package com.multi.runrunbackend.domain.course.dto.res;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author : kyungsoo
+ * @description : Please explain the class!!!
+ * @filename : CourseCreateRes
+ * @since : 2025. 12. 18. Thursday
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CourseCreateResDto {
+
+    private Long id;
+}
