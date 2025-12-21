a7ce9b1ee38812320dcc0678ccc10696155dea60
kyungsoo <yksoo00@naver.com>
2025-12-22 01:11:23 +0900

    feat(PathController) :path 추가

--- src/main/java/com/multi/runrunbackend/common/path/PathController.java
+++ src/main/java/com/multi/runrunbackend/common/path/PathController.java
@@ -1,10 +1,15 @@
 package com.multi.runrunbackend.common.path;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
+@RequiredArgsConstructor
 public class PathController {
+
 
     @GetMapping("/login")
     public String loginView() {
@@ -14,6 +19,46 @@ public class PathController {
     @GetMapping("/signup")
     public String signupView() {
         return "auth/signup";
+    }
+
+    @GetMapping("/test")
+    public String testView() {
+        return "test";
+    }
+
+    @GetMapping("/test2")
+    public String test2View() {
+        return "test2";
+    }
+
+    @GetMapping("/course")
+    public String courseView() {
+        return "course/courseList";
+    }
+
+    @GetMapping("/courseCreate")
+    public String courseCreateView() {
+        return "course/courseCreate";
+    }
+
+    @GetMapping("/courseDetail/{course_id}")
+    public String courseDetailView(
+        @PathVariable(name = "course_id") Long courseId,
+        Model model
+    ) {
+
+        model.addAttribute("courseId", courseId);
+        return "course/courseDetail";
+    }
+
+    @GetMapping("/courseUpdate/{course_id}")
+    public String courseUpdateView(
+        @PathVariable(name = "course_id") Long courseId,
+        Model model
+    ) {
+
+        model.addAttribute("courseId", courseId);
+        return "course/courseUpdate";
     }
 
 }
