36568084cc1dd91deffbc7d5b18eb7a3017ee0ed
changwooo-hub <tiger80898714@gmail.com>
2025-12-22 00:20:29 +0900

    채팅관련 페이지 호출 추가

--- src/main/java/com/multi/runrunbackend/common/path/PathController.java
+++ src/main/java/com/multi/runrunbackend/common/path/PathController.java
@@ -6,14 +6,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PathController {
 
-    @GetMapping("/login")
-    public String loginView() {
-        return "auth/login";
-    }
+  @GetMapping("/login")
+  public String loginView() {
+    return "auth/login";
+  }
 
-    @GetMapping("/signup")
-    public String signupView() {
-        return "auth/signup";
-    }
+  @GetMapping("/signup")
+  public String signupView() {
+    return "auth/signup";
+  }
 
+
+  @GetMapping("/chat")
+  public String chatList() {
+    return "chat/chat-list";
+  }
+
+  @GetMapping("/chat/chat1")
+  public String chat1() {
+    return "chat/chat1";
+  }
 }
