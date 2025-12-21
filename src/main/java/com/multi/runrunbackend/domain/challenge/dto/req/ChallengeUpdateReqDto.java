1fa05beb21959fbd589ace606ba13fc4f5adf288
YongWon Kim <38yong1@gmail.com>
2025-12-21 22:20:58 +0900

    feat: Challenge 관련 사용자의 참여 정보를 조회하기 위한 리포지토리 생성

---
+++ src/main/java/com/multi/runrunbackend/domain/challenge/dto/req/ChallengeUpdateReqDto.java
@@ -0,0 +1,46 @@
+package com.multi.runrunbackend.domain.challenge.dto.req;
+
+import com.multi.runrunbackend.domain.challenge.constant.ChallengeType;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author : kimyongwon
+ * @description : Please explain the class!!!
+ * @filename : ChallengeUpdateReqDto
+ * @since : 25. 12. 21. 오후 10:09 일요일
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class ChallengeUpdateReqDto {
+
+    @NotBlank(message = "챌린지 제목은 필수입니다.")
+    private String title;
+
+    @NotNull(message = "챌린지 타입은 필수입니다.")
+    private ChallengeType challengeType;
+
+    @NotNull(message = "목표값은 필수입니다.")
+    private Double targetValue;
+
+    @NotBlank(message = "설명은 필수입니다.")
+    private String description;
+
+    @NotNull(message = "시작일은 필수입니다.")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+
+    @NotNull(message = "종료일은 필수입니다.")
+    @FutureOrPresent(message = "종료일은 현재보다 과거일 수 없습니다.")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
+}
