4c30124676d85dd85bc269b5467771b758c8eea3
kyungsoo <yksoo00@naver.com>
2025-12-22 13:31:26 +0900

    feat(CourseRepository) : 코스 repository에 increase, decrease 구현(count+=1)

--- src/main/java/com/multi/runrunbackend/domain/course/repository/CourseRepository.java
+++ src/main/java/com/multi/runrunbackend/domain/course/repository/CourseRepository.java
@@ -1,7 +1,10 @@
 package com.multi.runrunbackend.domain.course.repository;
 
 import com.multi.runrunbackend.domain.course.entity.Course;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * @author : kyungsoo
@@ -11,5 +14,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
+    @Modifying
+    @Query("""
+            update Course c
+            set c.likeCount = c.likeCount + 1
+            where c.id = :courseId
+        """)
+    int increaseLikeCount(@Param("courseId") Long courseId);
 
+    @Modifying
+    @Query("""
+            update Course c
+            set c.likeCount = 
+                case when c.likeCount > 0 then c.likeCount - 1 else 0 end
+            where c.id = :courseId
+        """)
+    int decreaseLikeCount(@Param("courseId") Long courseId);
 }
