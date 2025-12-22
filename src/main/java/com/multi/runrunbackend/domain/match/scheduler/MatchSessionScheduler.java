658b96ebdbac3b323c4354cf7715ae6e9d709f90
KIMGWANGHO <kkh185923@naver.com>
2025-12-22 17:40:08 +0900

    feat(MatchSessionScheduler.java): 시작 1시간 전 모집글 조회하는 스케줄러 클래스 생성

---
+++ src/main/java/com/multi/runrunbackend/domain/match/scheduler/MatchSessionScheduler.java
@@ -0,0 +1,46 @@
+package com.multi.runrunbackend.domain.match.scheduler;
+
+import com.multi.runrunbackend.domain.match.service.MatchSessionService;
+import com.multi.runrunbackend.domain.recruit.entity.Recruit;
+import com.multi.runrunbackend.domain.recruit.repository.RecruitRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : KIMGWANGHO
+ * @description : Please explain the class!!!
+ * @filename : MatchSessionScheduler
+ * @since : 2025-12-22 월요일
+ */
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class MatchSessionScheduler {
+
+  private final RecruitRepository recruitRepository;
+  private final MatchSessionService matchSessionService;
+
+  public void autoCreateMatchSession() {
+
+    LocalDateTime targetTime = LocalDateTime.now().plusHours(1);
+    List<Recruit> pendingRecruits = recruitRepository.findAllByIsDeletedFalseAndMeetingAtBefore(
+        targetTime);
+
+    if (pendingRecruits.isEmpty()) {
+      return;
+    }
+
+    for (Recruit recruit : pendingRecruits) {
+      try {
+        matchSessionService.createOfflineSession(recruit.getId(), recruit.getUser().getId());
+      } catch (Exception e) {
+
+      }
+    }
+  }
+
+}
