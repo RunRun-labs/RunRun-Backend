5d1be5fb7d4f223c052f0ba32e1bb6a9a1435b0b
DESKTOP-P7D06C6\hbk49 <hbok491@gmail.com>
2025-12-23 13:49:39 +0900

    rename(JoinStatus): 패키지 변경(전용 constant)에 따른 import 경로 수정

--- src/main/java/com/multi/runrunbackend/domain/crew/constant/JoinStatus.java
+++ src/main/java/com/multi/runrunbackend/domain/crew/constant/JoinStatus.java
@@ -2,14 +2,14 @@ package com.multi.runrunbackend.domain.crew.constant;
 
 /**
  * @author : BoKyung
- * @description : 크루 가입 신청 상태 ENUM
+ * @description : 가입 신청 상태 Enum (PENDING: 대기중, APPROVED: 승인, REJECTED: 거절, CANCELED: 취소)
  * @filename : JoinStatus
- * @since : 25. 12. 22. 월요일
+ * @since : 25. 12. 17. 수요일
  */
 public enum JoinStatus {
-    PENDING,    // 대기중
-    APPROVED,   // 승인됨
-    REJECTED,   // 거절됨
-    CANCELED    // 취소됨
 
+    PENDING,
+    APPROVED,
+    REJECTED,
+    CANCELED
 }
