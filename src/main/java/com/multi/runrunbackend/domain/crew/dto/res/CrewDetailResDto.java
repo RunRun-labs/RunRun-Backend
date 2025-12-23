b32dd54fe1ff922afcdea2984c8e0fb843fc42ce
DESKTOP-P7D06C6\hbk49 <hbok491@gmail.com>
2025-12-23 13:56:14 +0900

    rename(CrewDetailResDto): 크루 관련 ENUM 파일 패키지 변경(전용 constant)에 따른 import 경로 수정

--- src/main/java/com/multi/runrunbackend/domain/crew/dto/res/CrewDetailResDto.java
+++ src/main/java/com/multi/runrunbackend/domain/crew/dto/res/CrewDetailResDto.java
@@ -1,8 +1,8 @@
 package com.multi.runrunbackend.domain.crew.dto.res;
 
+import com.multi.runrunbackend.domain.crew.constant.CrewRecruitStatus;
+import com.multi.runrunbackend.domain.crew.constant.CrewStatus;
 import com.multi.runrunbackend.domain.crew.entity.Crew;
-import com.multi.runrunbackend.domain.crew.entity.CrewRecruitStatus;
-import com.multi.runrunbackend.domain.crew.entity.CrewStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
