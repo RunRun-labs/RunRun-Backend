c67ed371bd37016b9daafdd01fabe7336b9d0a68
DESKTOP-P7D06C6\hbk49 <hbok491@gmail.com>
2025-12-23 13:42:18 +0900

    feat(CrewDistanceType): 크루 거리 타입 ENUM 추가

---
+++ src/main/java/com/multi/runrunbackend/domain/crew/constant/CrewDistanceType.java
@@ -0,0 +1,22 @@
+package com.multi.runrunbackend.domain.crew.constant;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author : BoKyung
+ * @description : 크루 관련 거리정보 ENUM (3km 미만, 3km, 5km, 10km, 10km 이상)
+ * @filename : CrewDistanceType
+ * @since : 2025-12-22 월요일
+ */
+@Getter
+@RequiredArgsConstructor
+public enum CrewDistanceType {
+    UNDER_3KM("3km 미만"),
+    KM_3("3km"),
+    KM_5("5km"),
+    KM_10("10km"),
+    OVER_10KM("10km 이상");
+
+    private final String description;
+}
