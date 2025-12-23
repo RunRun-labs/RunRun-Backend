194d9b52c19da8be61b04a110b0fdf440a7e8754
DESKTOP-P7D06C6\hbk49 <hbok491@gmail.com>
2025-12-23 23:11:15 +0900

    comment(JoinStatus): description 주석 설명 수정

--- src/main/java/com/multi/runrunbackend/domain/crew/constant/JoinStatus.java
+++ src/main/java/com/multi/runrunbackend/domain/crew/constant/JoinStatus.java
@@ -2,7 +2,7 @@ package com.multi.runrunbackend.domain.crew.constant;
 
 /**
  * @author : BoKyung
- * @description : 가입 신청 상태 Enum (PENDING: 대기중, APPROVED: 승인, REJECTED: 거절, CANCELED: 취소)
+ * @description : DB 저장용 - 가입 신청 상태 Enum (PENDING: 대기중, APPROVED: 승인, REJECTED: 거절, CANCELED: 취소)
  * @filename : JoinStatus
  * @since : 25. 12. 17. 수요일
  */
