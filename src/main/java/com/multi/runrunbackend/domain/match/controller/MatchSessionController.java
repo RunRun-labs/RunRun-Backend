d4914ab72c320d86fcbda6935c39f9b2b4e66357
KIMGWANGHO <kkh185923@naver.com>
2025-12-28 20:59:20 +0900

    feat(MatchSessionController): POST /online/cancel -> DELETE /online/join

--- src/main/java/com/multi/runrunbackend/domain/match/controller/MatchSessionController.java
+++ src/main/java/com/multi/runrunbackend/domain/match/controller/MatchSessionController.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -62,7 +63,7 @@ public class MatchSessionController {
     return ResponseEntity.ok(ApiResponse.success(response));
   }
 
-  @PostMapping("/online/cancel")
+  @DeleteMapping("/online/join")
   public ResponseEntity<ApiResponse<Void>> cancelOnlineMatch(
       @AuthenticationPrincipal CustomUser principal
   ) {
