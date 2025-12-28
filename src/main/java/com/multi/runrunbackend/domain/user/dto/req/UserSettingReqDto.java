a9e318a6f14350241ec590b2d78a2135ec6f1972
YongWon Kim <38yong1@gmail.com>
2025-12-28 22:13:33 +0900

    feat: 알림 상태 변경 값 전달하는 수정요청 DTO 추가

---
+++ src/main/java/com/multi/runrunbackend/domain/user/dto/req/UserSettingReqDto.java
@@ -0,0 +1,11 @@
+package com.multi.runrunbackend.domain.user.dto.req;
+
+/**
+ *
+ * @author : kimyongwon
+ * @description : Please explain the class!!!
+ * @filename : UserSettingReqDto
+ * @since : 25. 12. 28. 오후 10:11 일요일
+ */
+public class UserSettingReqDto {
+}
