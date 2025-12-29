e2d4f34104388ee39620f1f2ceb333fea948b556
DESKTOP-P7D06C6\hbk49 <hbok491@gmail.com>
2025-12-30 04:17:34 +0900

    feat(ErrorCode): 크루장 위임관련 errorcode 추가

--- src/main/java/com/multi/runrunbackend/common/exception/dto/ErrorCode.java
+++ src/main/java/com/multi/runrunbackend/common/exception/dto/ErrorCode.java
@@ -48,6 +48,7 @@ public enum ErrorCode {
     CREW_MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "CR015", "크루원을 찾을 수 없습니다."),
     CREW_RECRUITMENT_CLOSED(HttpStatus.CONFLICT, "CR016", "모집이 마감된 크루입니다."),
     NOT_CREW_USER(HttpStatus.FORBIDDEN, "CR017", "크루원이 아닙니다."),
+    CANNOT_ASSIGN_LEADER_TO_MEMBER(HttpStatus.FORBIDDEN, "CR018", "일반 멤버는 크루장이 될 수 없습니다. 부크루장 또는 운영진에게만 위임 가능합니다."),
     /*==== 파일 ====*/
     FILE_UPLOAD_FAILED(
 
