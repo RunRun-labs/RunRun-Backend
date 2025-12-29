05462dd7bbbf020e1ee2310b7f21d118e0f51392
YongWon Kim <38yong1@gmail.com>
2025-12-29 17:33:26 +0900

    refactor: 이용약관조회 path 불필요한 매개변수 제거

--- src/main/java/com/multi/runrunbackend/common/path/PathController.java
+++ src/main/java/com/multi/runrunbackend/common/path/PathController.java
@@ -4,45 +4,44 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PathController {
 
-  @GetMapping("/login")
-  public String loginView() {
-    return "auth/login";
-  }
+    @GetMapping("/login")
+    public String loginView() {
+        return "auth/login";
+    }
 
-  @GetMapping("/signup")
-  public String signupView() {
-    return "auth/signup";
-  }
+    @GetMapping("/signup")
+    public String signupView() {
+        return "auth/signup";
+    }
 
-  @GetMapping("/match/select")
-  public String matchSelectView() {
-    return "match/match-select";
-  }
+    @GetMapping("/match/select")
+    public String matchSelectView() {
+        return "match/match-select";
+    }
 
-  @GetMapping("/recruit")
-  public String recruitListView() {
-    return "recruit/recruit-list";
-  }
+    @GetMapping("/recruit")
+    public String recruitListView() {
+        return "recruit/recruit-list";
+    }
 
-  @GetMapping("/recruit/create")
-  public String recruitCreateView() {
-    return "recruit/recruit-create";
-  }
+    @GetMapping("/recruit/create")
+    public String recruitCreateView() {
+        return "recruit/recruit-create";
+    }
 
-  @GetMapping("/recruit/{id}")
-  public String recruitDetailView() {
-    return "recruit/recruit-detail";
-  }
+    @GetMapping("/recruit/{id}")
+    public String recruitDetailView() {
+        return "recruit/recruit-detail";
+    }
 
-  @GetMapping("/recruit/{id}/update")
-  public String recruitUpdateView() {
-    return "recruit/recruit-update";
-  }
+    @GetMapping("/recruit/{id}/update")
+    public String recruitUpdateView() {
+        return "recruit/recruit-update";
+    }
 
     @GetMapping("/crews")
     public String crewListPage() {
@@ -76,15 +75,16 @@ public class PathController {
         return "crew/crewJoinRequestList";
     }
 
-  @GetMapping("/chat")
-  public String chatList() {
-    return "chat/chat-list";
-  }
+    @GetMapping("/chat")
+    public String chatList() {
+        return "chat/chat-list";
+    }
 
-  @GetMapping("/chat/chat1")
-  public String chat1() {
-    return "chat/chat1";
-  }
+    @GetMapping("/chat/chat1")
+    public String chat1() {
+        return "chat/chat1";
+    }
+
     @GetMapping("/myPage")
     public String myPageView() {
         return "mypage/mypage";
@@ -144,8 +144,8 @@ public class PathController {
 
     @GetMapping("/courseDetail/{course_id}")
     public String courseDetailView(
-        @PathVariable(name = "course_id") Long courseId,
-        Model model
+            @PathVariable(name = "course_id") Long courseId,
+            Model model
     ) {
 
         model.addAttribute("courseId", courseId);
@@ -154,18 +154,18 @@ public class PathController {
 
     @GetMapping("/courseUpdate/{course_id}")
     public String courseUpdateView(
-        @PathVariable(name = "course_id") Long courseId,
-        Model model
+            @PathVariable(name = "course_id") Long courseId,
+            Model model
     ) {
 
         model.addAttribute("courseId", courseId);
         return "course/courseUpdate";
     }
 
-  @GetMapping("/test/gps")
-  public String gpsTestView() {
-    return "test/gps-test";
-  }
+    @GetMapping("/test/gps")
+    public String gpsTestView() {
+        return "test/gps-test";
+    }
 
     @GetMapping("/tts-test")
     public String ttsTestView() {
@@ -183,7 +183,7 @@ public class PathController {
     }
 
     @GetMapping("/terms/view")
-    public String termsView(@RequestParam(defaultValue = "SERVICE") String type, Model model) {
+    public String termsView() {
         return "terms/terms-detail";
     }
 
