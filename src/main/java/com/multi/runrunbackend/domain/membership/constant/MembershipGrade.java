14404363f9c629a6b947ca53467dc6e97ad5e0d1
DESKTOP-P7D06C6\hbk49 <hbok491@gmail.com>
2025-12-30 23:56:02 +0900

    feat(MembershipGrade): 멤버십 등급 ENUM 파일

---
+++ src/main/java/com/multi/runrunbackend/domain/membership/constant/MembershipGrade.java
@@ -0,0 +1,13 @@
+package com.multi.runrunbackend.domain.membership.constant;
+
+/**
+ * @author : BoKyung
+ * @description : 멤버십 등급을 관리하는 열거형 (Enum)-FREE(무료), PREMIUM(프리미엄)
+ * @filename : MembershipGrade
+ * @since : 25. 12. 30. 월요일
+ */
+public enum MembershipGrade {
+
+    FREE,      // 무료 회원
+    PREMIUM    // 프리미엄 회원
+}
