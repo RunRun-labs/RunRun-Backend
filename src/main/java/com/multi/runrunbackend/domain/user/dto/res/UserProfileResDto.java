f8c06dc802bc02e010556d7800b5920ebee9e91f
YongWon Kim <38yong1@gmail.com>
2025-12-30 16:31:36 +0900

    feat: 상대 프로필 조회시 id 필드 추가

--- src/main/java/com/multi/runrunbackend/domain/user/dto/res/UserProfileResDto.java
+++ src/main/java/com/multi/runrunbackend/domain/user/dto/res/UserProfileResDto.java
@@ -18,14 +18,15 @@ import lombok.Getter;
 public class UserProfileResDto {
 
     private Long id;
+    private String loginId;
     private String name;
     private String profileImageUrl;
 
     public static UserProfileResDto from(User user) {
-
         if (user.isDeleted()) {
             return UserProfileResDto.builder()
                     .id(user.getId())
+                    .loginId(user.getLoginId()) // User 엔티티의 로그인 ID getter에 맞게 수정
                     .name("탈퇴한 회원입니다")
                     .profileImageUrl(null)
                     .build();
@@ -33,6 +34,7 @@ public class UserProfileResDto {
 
         return UserProfileResDto.builder()
                 .id(user.getId())
+                .loginId(user.getLoginId())     // User 엔티티의 필드명이 다르면 여기만 바꾸면 됨
                 .name(user.getName())
                 .profileImageUrl(user.getProfileImageUrl())
                 .build();
