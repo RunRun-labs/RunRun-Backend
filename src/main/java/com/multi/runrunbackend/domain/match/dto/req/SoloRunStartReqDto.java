d0f8eb26813f04dd14cc86470d384f08f0ee9cc8
KIMGWANGHO <kkh185923@naver.com>
2026-01-02 14:29:47 +0900

    feat(SoloRunStartReqDto): 솔로런 세션 생성 request dto 작성

---
+++ src/main/java/com/multi/runrunbackend/domain/match/dto/req/SoloRunStartReqDto.java
@@ -0,0 +1,22 @@
+package com.multi.runrunbackend.domain.match.dto.req;
+
+import com.multi.runrunbackend.common.constant.DistanceType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author : KIMGWANGHO
+ * @description : Please explain the class!!!
+ * @filename : SoloRunStartReqDto
+ * @since : 2026-01-01 목요일
+ */
+@Getter
+@NoArgsConstructor
+public class SoloRunStartReqDto {
+
+  private DistanceType distance;
+
+  private Long courseId;
+
+
+}
