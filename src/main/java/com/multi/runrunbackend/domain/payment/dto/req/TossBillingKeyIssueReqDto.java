88139b5f1b5b5527f83173702f5dfbbfc77e3d07
DESKTOP-P7D06C6\hbk49 <hbok491@gmail.com>
2026-01-03 03:06:13 +0900

    feat(TossBillingKeyIssueReqDto): 유효성 검증 어노테이션 추가

--- src/main/java/com/multi/runrunbackend/domain/payment/dto/req/TossBillingKeyIssueReqDto.java
+++ src/main/java/com/multi/runrunbackend/domain/payment/dto/req/TossBillingKeyIssueReqDto.java
@@ -1,6 +1,7 @@
 package com.multi.runrunbackend.domain.payment.dto.req;
 
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,6 +18,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TossBillingKeyIssueReqDto {
+    @NotBlank(message = "카드 인증 키는 필수입니다")
     private String authKey;        // 카드 인증 키 (프론트에서 받음)
+
+    @NotBlank(message = "사용자 키는 필수입니다")
     private String customerKey;    // 사용자 키 (userId)
 }
