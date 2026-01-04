1183e70a028fb04fbbfeb2badd07dcdb0331c758
YongWon Kim <38yong1@gmail.com>
2026-01-04 19:10:41 +0900

    feat: 오늘의 러닝 정보 응답을 위한 DTO추가

--- src/main/java/com/multi/runrunbackend/domain/match/dto/res/TodayRunningSummaryResDto.java
+++ src/main/java/com/multi/runrunbackend/domain/match/dto/res/TodayRunningSummaryResDto.java
@@ -1,19 +1,30 @@
 package com.multi.runrunbackend.domain.match.dto.res;
 
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.math.BigDecimal;
 
 /**
  *
  * @author : kimyongwon
- * @description : Please explain the class!!!
+ * @description : 오늘의 러닝 요약 정보 응답 DTO
  * @filename : TodayRunningSummaryResDto
  * @since : 26. 1. 3. 오후 5:25 토요일
  */
 @Getter
-@Builder
+@AllArgsConstructor
 public class TodayRunningSummaryResDto {
-    private double distanceKm;
-    private int durationSec;
-    private int calories;
+
+    private BigDecimal distanceKm;
+    private Integer durationSec;
+    private Integer calories;
+
+    public static TodayRunningSummaryResDto from(
+            BigDecimal distanceKm,
+            Integer durationSec,
+            Integer calories
+    ) {
+        return new TodayRunningSummaryResDto(distanceKm, durationSec, calories);
+    }
 }
