67b13bb29e98be295c627e74763e5a24933154e1
YongWon Kim <38yong1@gmail.com>
2026-01-05 23:49:16 +0900

    feat: 타인의 러닝 기록 조회 로직 추가(차단시 볼수 없는 로직 포함)

--- src/main/java/com/multi/runrunbackend/domain/match/service/ProfileRunningHistoryService.java
+++ src/main/java/com/multi/runrunbackend/domain/match/service/ProfileRunningHistoryService.java
@@ -1,13 +1,14 @@
 package com.multi.runrunbackend.domain.match.service;
 
+import com.multi.runrunbackend.common.exception.custom.ForbiddenException;
 import com.multi.runrunbackend.common.exception.custom.NotFoundException;
 import com.multi.runrunbackend.common.exception.custom.TokenException;
 import com.multi.runrunbackend.common.exception.dto.ErrorCode;
 import com.multi.runrunbackend.domain.auth.dto.CustomUser;
-import com.multi.runrunbackend.domain.match.constant.RunStatus;
 import com.multi.runrunbackend.domain.match.dto.res.ProfileRunningHistoryResDto;
 import com.multi.runrunbackend.domain.match.repository.RunningResultRepository;
 import com.multi.runrunbackend.domain.user.entity.User;
+import com.multi.runrunbackend.domain.user.repository.UserBlockRepository;
 import com.multi.runrunbackend.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -18,7 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 /**
  *
  * @author : kimyongwon
- * @description : 마이페이지 - 내 러닝 기록 조회 Service
+ * @description : 마이페이지 - 러닝 기록 조회 Service
  * @filename : ProfileRunningHistoryService
  * @since : 26. 1. 4. 오후 7:29 일요일
  */
@@ -29,19 +30,43 @@ public class ProfileRunningHistoryService {
 
     private final RunningResultRepository runningResultRepository;
     private final UserRepository userRepository;
+    private final UserBlockRepository userBlockRepository;
 
+    /**
+     * 내 러닝 기록 조회
+     */
     public Slice<ProfileRunningHistoryResDto> getMyRunningRecords(
             CustomUser principal,
             Pageable pageable
     ) {
-        User user = getUserByPrincipal(principal);
+        User me = getUserByPrincipal(principal);
 
         return runningResultRepository
-                .findByUserIdAndRunStatus(
-                        user.getId(),
-                        RunStatus.COMPLETED,
-                        pageable
-                )
+                .findCompletedByUser(me, pageable)
+                .map(ProfileRunningHistoryResDto::from);
+    }
+
+    /**
+     * 타 사용자 러닝 기록 조회
+     */
+    public Slice<ProfileRunningHistoryResDto> getUserRunningRecords(
+            Long userId,
+            CustomUser principal,
+            Pageable pageable
+    ) {
+        User me = getUserByPrincipal(principal);
+
+        User target = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND));
+
+
+        if (userBlockRepository.existsByBlockerAndBlockedUser(me, target)
+                || userBlockRepository.existsByBlockerAndBlockedUser(target, me)) {
+            throw new ForbiddenException(ErrorCode.USER_BLOCKED);
+        }
+
+        return runningResultRepository
+                .findCompletedByUser(target, pageable)
                 .map(ProfileRunningHistoryResDto::from);
     }
 
