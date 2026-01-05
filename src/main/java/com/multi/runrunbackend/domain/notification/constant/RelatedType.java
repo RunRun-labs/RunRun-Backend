c81a24d1176734e24a1c9abe2618c3ef54afc0ee
KIMGWANGHO <kkh185923@naver.com>
2026-01-06 01:03:39 +0900

    feat(RelatedType): 프론트에서 라우팅할 enum 필드 추가

--- src/main/java/com/multi/runrunbackend/domain/notification/constant/RelatedType.java
+++ src/main/java/com/multi/runrunbackend/domain/notification/constant/RelatedType.java
@@ -7,5 +7,41 @@ package com.multi.runrunbackend.domain.notification.constant;
  * @since : 2025-12-17 수요일
  */
 public enum RelatedType {
-    ROOM, CREW
+  CREW_CHAT_ROOM,
+
+  RECRUIT,
+
+  OFFLINE_CHAT_ROOM,
+  ONLINE_BATTLE,
+  SOLORUN,
+  GHOSTRUN,
+
+  BATTLE_RESULT,
+  RUNNING_RESULT,
+
+  CHALLENGE,
+  FEED_POST,
+
+  CREW,
+  CREW_JOIN_REQUEST,
+  CREW_USER,
+  CREW_ACTIVITY,
+  CREW_ACTIVITY_USER,
+
+  MEMBERSHIP,
+
+  PAYMENT,
+
+  USER_POINT,
+  POINT_HISTORY,
+  POINT_EXPIRATION,
+  POINT_PRODUCT,
+
+  RATING,
+
+  AD,
+  COUPON,
+  COURSE,
+
+  USERS
 }
