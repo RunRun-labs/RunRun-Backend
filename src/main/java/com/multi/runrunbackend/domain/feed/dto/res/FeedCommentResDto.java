0e06bfebcd067dd8fabd98c7dc0b1b2277a90952
YongWon Kim <38yong1@gmail.com>
2026-01-05 13:59:42 +0900

    feat: 댓글 조회시 응답 DTO 추가

---
+++ src/main/java/com/multi/runrunbackend/domain/feed/dto/res/FeedCommentResDto.java
@@ -0,0 +1,42 @@
+package com.multi.runrunbackend.domain.feed.dto.res;
+
+import com.multi.runrunbackend.domain.feed.entity.FeedComment;
+import com.multi.runrunbackend.domain.user.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author : kimyongwon
+ * @description : 댓글 조회 응답 DTO
+ * @filename : FeedCommentResDto
+ * @since : 26. 1. 5. 오후 1:44 월요일
+ */
+@Getter
+@Builder
+public class FeedCommentResDto {
+
+    private Long commentId;
+    private String content;
+
+    private Long userId;
+    private String userName;
+    private String profileImageUrl;
+
+    private LocalDateTime createdAt;
+
+    public static FeedCommentResDto from(FeedComment comment) {
+        User user = comment.getUser();
+
+        return FeedCommentResDto.builder()
+                .commentId(comment.getId())
+                .content(comment.getContent())
+                .userId(user.getId())
+                .userName(user.getName())
+                .profileImageUrl(user.getProfileImageUrl())
+                .createdAt(comment.getCreatedAt())
+                .build();
+    }
+}
