3b47cedc2a9fff6063a3c3b1b0617a56b2ca59d5
KIMGWANGHO <kkh185923@naver.com>
2026-01-06 01:06:46 +0900

    feat(NotificationRepository): NotificationRepository 구현

---
+++ src/main/java/com/multi/runrunbackend/domain/notification/repository/NotificationRepository.java
@@ -0,0 +1,19 @@
+package com.multi.runrunbackend.domain.notification.repository;
+
+import com.multi.runrunbackend.domain.notification.entity.Notification;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author : KIMGWANGHO
+ * @description : Please explain the class!!!
+ * @filename : NotificationRepository
+ * @since : 2026-01-05 월요일
+ */
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+  Slice<Notification> findByReceiver_IdAndIsDeletedFalse(Long receiverId, Pageable pageable);
+
+
+}
