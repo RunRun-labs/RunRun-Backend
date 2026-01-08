793e46fe71838ccb462a234ba8f364c78aa06ef2
YongWon Kim <38yong1@gmail.com>
2026-01-08 17:06:07 +0900

    feat: 내 쿠폰함 path 연결

--- src/main/resources/static/js/mypage.js
+++ src/main/resources/static/js/mypage.js
@@ -189,7 +189,7 @@ function attachCouponsHandler() {
     if (!couponsBtn) return;
 
     couponsBtn.addEventListener("click", () => {
-        window.location.href = "/coupons";
+        window.location.href = "/coupon/my";
     });
 }
 
