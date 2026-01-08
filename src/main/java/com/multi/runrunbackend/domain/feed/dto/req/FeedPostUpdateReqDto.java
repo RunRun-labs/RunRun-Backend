66a69d8f11cc44bc80ab79b835817c0b7c642768
YongWon Kim <38yong1@gmail.com>
2026-01-08 09:56:22 +0900

    feat: 피드 수정시 Validation 검증 커스텀 메시지 추가

--- src/main/java/com/multi/runrunbackend/domain/feed/dto/req/FeedPostUpdateReqDto.java
+++ src/main/java/com/multi/runrunbackend/domain/feed/dto/req/FeedPostUpdateReqDto.java
@@ -16,8 +16,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class FeedPostUpdateReqDto {
 
-    @NotBlank
-    @Size(max = 500)
+    @NotBlank(message = "피드 내용은 비어 있을 수 없습니다.")
+    @Size(max = 500, message = "피드 내용은 최대 500자까지 작성할 수 있습니다.")
     private String content;
 
 }
