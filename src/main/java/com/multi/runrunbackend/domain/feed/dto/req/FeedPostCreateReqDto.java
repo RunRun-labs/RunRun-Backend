0bf833d37055ef5f22779c8a633e96e70f92d40f
YongWon Kim <38yong1@gmail.com>
2026-01-08 09:53:54 +0900

    feat: 피드 생성시 수정과 동일하게 NotBlank 으로 일관적인 검증

--- src/main/java/com/multi/runrunbackend/domain/feed/dto/req/FeedPostCreateReqDto.java
+++ src/main/java/com/multi/runrunbackend/domain/feed/dto/req/FeedPostCreateReqDto.java
@@ -1,5 +1,6 @@
 package com.multi.runrunbackend.domain.feed.dto.req;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -19,6 +20,7 @@ public class FeedPostCreateReqDto {
     @NotNull
     private Long runningResultId;
 
+    @NotBlank
     @Size(max = 500)
     private String content;
 }
