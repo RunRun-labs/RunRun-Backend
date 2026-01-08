733951a8955bf79952c338814b1200add61db818
YongWon Kim <38yong1@gmail.com>
2026-01-08 15:13:17 +0900

    feat: 이용약관 모달 UI 수정

--- src/main/java/com/multi/runrunbackend/domain/term/constant/TermsType.java
+++ src/main/java/com/multi/runrunbackend/domain/term/constant/TermsType.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 /**
  *
  * @author : kimyongwon
- * @description : 약관 타입 관리 Enum (서비스 이용약관, 개인정보처리방침, 마케팅 수신동의)
+ * @description : 약관 타입 관리 Enum (서비스 이용약관, 개인정보처리방침, 유료 서비스 이용약관)
  * @filename : TermsType
  * @since : 25. 12. 17. 오후 1:24 수요일
  */
