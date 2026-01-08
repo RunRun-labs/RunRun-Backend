fe2f2db7d408d838c0a373e6224a1a70fa8ecb85
YongWon Kim <38yong1@gmail.com>
2026-01-08 15:09:43 +0900

    feat: 약관타입 명칭 변경

--- src/main/java/com/multi/runrunbackend/domain/term/constant/TermsType.java
+++ src/main/java/com/multi/runrunbackend/domain/term/constant/TermsType.java
@@ -15,7 +15,7 @@ import lombok.RequiredArgsConstructor;
 public enum TermsType {
     SERVICE("서비스 이용약관"),
     PRIVACY("개인정보 처리방침"),
-    MARKETING("마케팅 정보 수신 동의");
+    MARKETING("유료 서비스 이용약관");
 
     private final String description;
 }
