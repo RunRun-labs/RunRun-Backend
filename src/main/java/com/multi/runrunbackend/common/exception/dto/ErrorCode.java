8c07ed26f33fb26f2612076c064a30e637488c45
YongWon Kim <38yong1@gmail.com>
2026-01-08 09:39:56 +0900

    feat: 중복된 에러코드 수정

--- src/main/java/com/multi/runrunbackend/common/exception/dto/ErrorCode.java
+++ src/main/java/com/multi/runrunbackend/common/exception/dto/ErrorCode.java
@@ -348,11 +348,11 @@ public enum ErrorCode {
     AGE_RESTRICTION(HttpStatus.BAD_REQUEST, "R015", "참여 가능 나이가 아닙니다."),
     GENDER_RESTRICTION(HttpStatus.BAD_REQUEST, "R016", "참여 가능 성별이 아닙니다."),
     BIRTHDATE_REQUIRED(HttpStatus.BAD_REQUEST, "R017", "권한이 없는 나이대 입니다."),
-    /*=====매칭=====*/
-    RUNNING_RESULT_NOT_FOUND(HttpStatus.NOT_FOUND, "M001", "러닝 결과를 찾을 수 없습니다."),
-    RUNNING_RESULT_ALREADY_DELETED(HttpStatus.BAD_REQUEST, "M002", "이미 삭제된 러닝 결과입니다."),
-    RUNNING_RESULT_FORBIDDEN(HttpStatus.FORBIDDEN, "M003", "해당 러닝 기록에 대한 권한이 없습니다."),
-    /*===== 배틀 결과 =====*/
+    /*=====러닝결과=====*/
+    RUNNING_RESULT_NOT_FOUND(HttpStatus.NOT_FOUND, "RUN001", "러닝 결과를 찾을 수 없습니다."),
+    RUNNING_RESULT_ALREADY_DELETED(HttpStatus.BAD_REQUEST, "RUN002", "이미 삭제된 러닝 결과입니다."),
+    RUNNING_RESULT_FORBIDDEN(HttpStatus.FORBIDDEN, "RUN003", "해당 러닝 기록에 대한 권한이 없습니다."),
+    /*===== 배틀/매칭 =====*/
     BATTLE_RESULT_NOT_FOUND(HttpStatus.NOT_FOUND, "B001", "배틀 결과를 찾을 수 없습니다."),
     DISTANCE_REQUIRED(HttpStatus.BAD_REQUEST, "M002", "코스 선택 안할 시 거리 선택은 필수 입니다."),
     INVALID_DISTANCE_TYPE(HttpStatus.BAD_REQUEST, "M003", "유효하지 않은 거리 타입입니다."),
