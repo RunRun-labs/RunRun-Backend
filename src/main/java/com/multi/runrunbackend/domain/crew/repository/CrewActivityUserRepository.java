d2dea4d7ac01fdf8a9af3134b881fcff3a799d69
DESKTOP-P7D06C6\hbk49 <hbok491@gmail.com>
2026-01-11 23:58:43 +0900

    feat(CrewActivityUserRepository): 크루 활동 참여자 Repository

---
+++ src/main/java/com/multi/runrunbackend/domain/crew/repository/CrewActivityUserRepository.java
@@ -0,0 +1,25 @@
+package com.multi.runrunbackend.domain.crew.repository;
+
+import com.multi.runrunbackend.domain.crew.entity.CrewActivityUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * @author : BoKyung
+ * @description : 크루 활동 참여자 Repository
+ * @filename : CrewActivityUserRepository
+ * @since : 25. 1. 11. 일요일
+ */
+public interface CrewActivityUserRepository extends JpaRepository<CrewActivityUser, Long> {
+
+    /**
+     * @description : 특정 활동의 참여자 목록 조회
+     */
+    List<CrewActivityUser> findByCrewActivityId(Long activityId);
+
+    /**
+     * @description : 특정 활동의 참여자 전체 삭제
+     */
+    void deleteByCrewActivityId(Long activityId);
+}
