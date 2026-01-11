78f9fe1b18e1236594366992121ffa738759dc57
kyungsoo <yksoo00@naver.com>
2026-01-12 00:24:27 +0900

    feat: 생성자 추가

--- src/main/java/com/multi/runrunbackend/domain/advertisement/dto/req/adclick/AdImpressionReqDto.java
+++ src/main/java/com/multi/runrunbackend/domain/advertisement/dto/req/adclick/AdImpressionReqDto.java
@@ -2,6 +2,8 @@ package com.multi.runrunbackend.domain.advertisement.dto.req.adclick;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * @author : kyungsoo
@@ -10,6 +12,8 @@ import lombok.Getter;
  * @since : 2026. 1. 11. Sunday
  */
 @Getter
+@Setter
+@NoArgsConstructor
 public class AdImpressionReqDto {
 
     @NotNull(message = "placementId는 필수입니다.")
