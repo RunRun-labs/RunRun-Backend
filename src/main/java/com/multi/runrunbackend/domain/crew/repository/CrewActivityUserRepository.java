4221e8be2c25e2c1cd3cb81eee23347085b7091a
DESKTOP-P7D06C6\hbk49 <hbok491@gmail.com>
2026-01-12 11:24:11 +0900

    feat(CrewActivityUserRepository): 삭제 쿼리에 @Modifying과 @transactional 어노테이션 추가

--- src/main/java/com/multi/runrunbackend/domain/crew/repository/CrewActivityUserRepository.java
+++ src/main/java/com/multi/runrunbackend/domain/crew/repository/CrewActivityUserRepository.java
@@ -2,6 +2,8 @@ package com.multi.runrunbackend.domain.crew.repository;
 
 import com.multi.runrunbackend.domain.crew.entity.CrewActivityUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -21,5 +23,7 @@ public interface CrewActivityUserRepository extends JpaRepository<CrewActivityUs
     /**
      * @description : 특정 활동의 참여자 전체 삭제
      */
+    @Modifying
+    @Transactional
     void deleteByCrewActivityId(Long activityId);
 }
